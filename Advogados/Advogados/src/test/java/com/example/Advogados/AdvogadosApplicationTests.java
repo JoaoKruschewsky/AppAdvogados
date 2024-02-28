e6e3612014a5abf4fa4afe7577bb2ec21b619442
Joao Kruschewsky <joaopedrokruschewsky123@gmail.com>
2024-02-27 22:27:59 -0300

    .

---
+++ Advogados/Advogados/src/test/java/com/example/Advogados/AdvogadosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Advogados;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvogadosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
