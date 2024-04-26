814acb3cb164465bbca1cd63251701976f7b8b2d
Joao Kruschewsky <joaopedrokruschewsky123@gmail.com>
2024-04-25 21:56:39 -0300

    .

--- Advogados/Advogados/src/main/java/com/example/Advogados/Services/interfaces/login/verifyLoginLawyer.java
+++ Advogados/Advogados/src/main/java/com/example/Advogados/Services/interfaces/login/verifyLoginLawyer.java
@@ -3,9 +3,9 @@ package com.example.Advogados.Services.interfaces.login;
 import org.springframework.http.ResponseEntity;
 
 import com.example.Advogados.Model.Lawyers;
+import com.example.Advogados.Model.User;
 
-public interface verifyLoginLawyer  {
+public interface verifyLoginLawyer {
 
-    
     public ResponseEntity<?> verifySaveLawyers(Lawyers Lawyers);
 }
